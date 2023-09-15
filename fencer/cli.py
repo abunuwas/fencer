9280c2d3f9858feb2c002424b273167a46146cd4
Eric-0522 <wfps4710@gmail.com>
2023-09-15 09:56:40 +0800

    將sql_injection和authorized_endpoints註解

--- fencer/cli.py
+++ fencer/cli.py
@@ -42,15 +42,15 @@ def run(oas_file, base_url):
   -------------------------
   Testing injection attacks
   -------------------------"""
-    click.echo(injection_message)
-    test_runner.run_sql_injection_attacks()
+    #click.echo(injection_message)
+    #test_runner.run_sql_injection_attacks()
 
     injection_message = """
   -------------------------
   Testing unauthorized access
   -------------------------"""
-    click.echo(injection_message)
-    test_runner.run_unauthorized_access_attacks()
+    #click.echo(injection_message)
+    #test_runner.run_unauthorized_access_attacks()
 
     idor_message = """
   -------------------------
