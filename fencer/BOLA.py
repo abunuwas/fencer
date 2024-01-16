f01d49aa6d9de5f8411ff0cfcaa6b163d27fb9d8
Eric-0522 <wfps4710@gmail.com>
2024-01-16 14:18:39 +0800

    新增properties_analyzer函式和attack_analyzer函式

--- fencer/BOLA.py
+++ fencer/BOLA.py
@@ -9,3 +9,18 @@ import requests
 from .api_spec import Endpoint, fake_parameter, APISpec
 from .test_case import TestResult, TestCase, AttackStrategy, TestDescription, HTTPMethods, VulnerabilitySeverityLevel
 
+class TestBOLA:
+    def __init__(self, api_spec: APISpec):
+        self.api_spec = api_spec
+
+    def properties_analyzer(self):
+        if self.api_spec.authorized_endpoints(): # 如果有Security authorization
+            if self.api_spec.paths:    #api_spc是否有Paths屬性
+                for path in self.api_spec.paths:
+                    pass
+            else:
+                return 
+        else:
+            return
+    def attack_analyzer(self):
+        pass
