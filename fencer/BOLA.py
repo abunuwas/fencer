820f8976850e84b84d014f129ec2c29adf85c864
Eric-0522 <wfps4710@gmail.com>
2024-01-18 15:10:11 +0800

    修改annotate_with_table_2_properties函式

--- fencer/BOLA.py
+++ fencer/BOLA.py
@@ -6,8 +6,9 @@ from typing import List, Callable, Optional
 import click
 import requests
 
-from .api_spec import Endpoint, fake_parameter, APISpec
-from .test_case import TestResult, TestCase, AttackStrategy, TestDescription, HTTPMethods, VulnerabilitySeverityLevel
+from .api_spec import APISpec
+from .test_case import TestResult, TestCase, AttackStrategy, TestDescription, HTTPMethods,\
+      VulnerabilitySeverityLevel
 
 standard_http_methods = ['get', 'post', 'put', 'patch', 'delete', 'options', 'head']
 
@@ -34,23 +35,34 @@ table_2_parameters_properties = {
 class TestBOLA:
     def __init__(self, api_spec: APISpec):
         self.api_spec = api_spec
-
-    def annotate_with_table_2_properties(item,table_2_parameters_properties):
+        self.paths = api_spec.paths
+    def is_identifier(self):
+        if len(self.api_spec.authorized_endpoints()) > 0:
+            return True
+        else:
+            return False
+        
+    def annotate_with_table_2_properties(self,item,table_2_properties):
+        
         if 'in' in item:
             parameter_location = item['in']
-        properties_to_annotate = table_2_parameters_properties.get(parameter_location,{})
-        item['x-properties'] = properties_to_annotate
+        #properties_to_annotate = table_2_parameters_properties.get(parameter_location,{})
+        item['Parameter-level-properties:']
+        item['is_identifier:'] = str(self.is_identifier())
+        item['Location:'] = table_2_parameters_properties[parameter_location]
+        item['type:'] = self.api_spec.schema['type']
+        
 
     def properties_analyzer(self):
         if self.api_spec.authorized_endpoints(): # 如果有Security authorization
-            if self.api_spec.paths:    #api_spc是否有Paths屬性
-                for path in self.api_spec.paths:
+            if self.paths:    #api_spc是否有Paths屬性
+                for path in self.paths:
                     if 'parameters' in path:
                         for parameters in path['parameters']:
                             self.annotate_with_table_2_properties(parameters,table_2_parameters_properties)
-                        #print(parameters)
-                    if self.api_spec.paths[path].keys() in standard_http_methods: # 檢查path item是否有operation物件(GET、POST etc)
-                        operation = self.api_spec.paths[path].keys() # 取得Http的Method
+                            #print(parameters)
+                    if self.paths[path].keys() in standard_http_methods: # 檢查path item是否有operation物件(GET、POST,etc)
+                        operation = self.paths[path].keys() # 取得Path中的Http_Method
                         if 'parameters' in operation:
                             parameters = path['parameters']
                             self.annotate_with_table_2_properties(parameters,table_2_parameters_properties)
