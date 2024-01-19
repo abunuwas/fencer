5106ecf0afc1bdd04bbbb7b6a05383c97182d907
Eric-0522 <wfps4710@gmail.com>
2024-01-19 15:51:56 +0800

    更新properties_analyzer函式和annotate_with_parameter函式

--- fencer/BOLA.py
+++ fencer/BOLA.py
@@ -31,46 +31,49 @@ table_2_parameters_properties = {
     }
 }
 
-
 class TestBOLA:
     def __init__(self, api_spec: APISpec):
         self.api_spec = api_spec
         self.paths = api_spec.paths
-
+        
     def is_identifier(self,parameter): # If parameter is_identifier then return True else return False
         if parameter.get('in') == 'path' and parameter.get('require',True):
             return True
         return False
         
-    def annotate_with_parameters_table_2_properties(self,item,table_2_properties):
+    def annotate_with_parameters_table2_properties(self,item,table_2_properties):
         if 'in' in item:
             parameter_location = item['in']
-        #properties_to_annotate = table_2_parameters_properties.get(parameter_location,{})
-        item['Parameter-level-properties:']
-        item['is_identifier:'] = str(self.is_identifier(item))
-        item['Location:'] = table_2_parameters_properties[parameter_location]
-        item['type:'] = self.api_spec.schema['type']
-
-    def annotate_with_operation_table_2_properties(self,operation):
+        parameter_level_properties = {
+            'is_identifier':self.is_identifier(item),
+            'Location':parameter_location,
+            'type':item['schema']['type']
+        }
+        item['Parameter-level-properties'] = parameter_level_properties
+        return item
+    
+    def annotate_with_operation_table2_properties(self,operation):
         pass
 
     def properties_analyzer(self):
-        if self.api_spec.authorized_endpoints(): # 如果有Security authorization
+        if 'securitySchemes' in self.api_spec.components: # 如果有Security authorization
             if self.paths:    #api_spc是否有Paths屬性
-                for path in self.paths:
-                    if 'parameters' in path:
-                        for parameters in path['parameters']:
-                            self.annotate_with_table_2_properties(parameters,table_2_parameters_properties)
-                            #print(parameters)
-                    if self.paths[path].keys() in standard_http_methods: # 檢查path item是否有operation物件(GET、POST,etc)
+                for path,path_data in self.paths.items(): # path代表API端點，而path_data代表此端點所包含的物件和屬性
+                    if 'parameters' in path_data:
+                        for parameters in path_data['parameters']:
+                          annotate_parameters = self.annotate_with_parameters_table2_properties(parameters,table_2_parameters_properties)
+                          print(annotate_parameters)
+                    """
+                    if any(method in standard_http_methods for method in self.paths[path].keys()): # 檢查path item是否有operation物件(GET、POST,etc)
                         operation = self.paths[path].keys() # 取得Path中的Http_Method
                         if 'parameters' in operation:
                             parameters = path['parameters']
-                            self.annotate_with_table_2_properties(parameters,table_2_parameters_properties)
+                            self.annotate_with_parameters_table_2_properties(parameters,table_2_parameters_properties)
                         else:
                             pass
+                    """
             else:
-                return 
+                print("沒有paths物件")
         else:
             return
         
