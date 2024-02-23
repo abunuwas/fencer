09a1efa1c85150141ac9a17c9418097c8805b452
Eric-0522 <wfps4710@gmail.com>
2024-02-23 01:58:40 -0500

    Need to revise create_fake_parameter_with_path_position function

--- fencer/BFLA.py
+++ fencer/BFLA.py
@@ -15,6 +15,40 @@ AUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhYmNAZ21haWwuY29tIiwicm9sZSI
                    'bzWTmWgIQfZUoh_1q0gSVS13k-dw5963kUlGeL5d5XZuSIvO0lVufjCoo7ASJOu39RZ2mv109ig-QuX5FOnDcKM9Zx1kh9gcJBoahZyw4khebVDN5M7Z39qE0'\
                    'c1EhuJyZ34BXy0Vgq5HvuBC0pdKfR7c9X3tTmK9vemxyqTM-ui1TU_ftr_1fOBOk9BIy7lyj3Zh49Mi9t4xhbUOIk3wfG3qMx3BaHQ'
 
+UNAUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjYmFAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDgzMjY3NTYsImV4cCI6MTcwODkzMTU1Nn0.'\
+                     'bCnedndWp2M1W2NLpke_J9X1AO_5h7we4lZDcT7In0J3AqCGdVNIJKllTV6VJA9c_5CXN3cvlfpaZiCB17pMH1PUAaVKtKTvSQQNIGmgNJ--WZN--30Sp00'\
+                     'kVc4-szaY8iR4PUZFw4CZO9PGBmpc-9nA4jUMR3Esj5DLE8ghpq1Gf7mYC1x3PVabSyvTk7JnRIfxnNOKlZb9B-0XiKRdU88URDiI9muHUgF8WC9EnNaYGhQO'\
+                     '-f1O-O5dJM2iQuKhvJX2GhBtiShxIXr618ImISgQEESG0XnVMnmOiKl69hfkgT8aVdS2KYR8Wg4MaXc5m2NgDV6l05VtCrW95XSSbQ'
+
+@dataclass
+class BFLAEndpoint:
+    endpoint:Endpoint
+    fake_param_strategy: Optional[Callable] = None
+    fake_payload_strategy = None
+
+    def __post_init__(self):
+        self.fake_param_strategy = (
+            self.fake_param_strategy or fake_parameter
+        )
+        self.fake_payload_strategy = (
+            self.fake_payload_strategy or JSF
+        )
+    def create_fake_parameter_with_path_position(self):
+        urls = []
+        for param in self.endpoint.path.path_params_list:
+            #print(" ",param)
+            path = self.endpoint.path.path.replace(f'{{{param}}}')
+
+    def gets_url_with_test_path_params(self):
+        urls = []
+        if self.endpoint.path.has_path_params():
+            urls.extend(self.create_fake_parameter_with_path_position())
+            if self.endpoint.has_required_query_params():
+                pass
+        for url in urls:
+            yield url
+
+
 class BFLATestCaseRunner:
     def __init__(self,test_case: TestCase):
         self.test_case = test_case
@@ -55,11 +89,44 @@ class TestBFLA:
         self.api_spec = api_spec
         self.auth_tests = 0
     def run_BFLA_attack_through_path_parameters(self):
-        pass
-
-    def test_BFLA_attack(self):
         failing_tests = []
         for endpoint in self.api_spec.authorized_endpoints:
+            if not endpoint.has_path_params():
+                continue
+            BFLA_attack = BFLAEndpoint(endpoint)
+            endpoint_failing_tests = []
+            click.echo(f"    {endpoint.method.upper()} {endpoint.base_url + endpoint.path.path}", nl=False)
+            for url in BFLA_attack.gets_url_with_test_path_params():
+                self.auth_tests += 1
+                BFLA_test_case = BFLATestCaseRunner(
+                    test_case = TestCase(
+                        category=AttackStrategy.BFLA,
+                        test_target="Broken_Function_Level_Authorization_attack",
+                        description=TestDescription(
+                            http_method=getattr(HTTPMethods, endpoint.method.upper()),
+                            url=endpoint.safe_url, base_url=endpoint.base_url, path=endpoint.path.path,
+                            payload=(
+                                endpoint.generate_safe_request_payload()
+                                if endpoint.has_request_payload() else None
+                            ),
+                        )
+                    )
+                )
+                BFLA_test_case.run(AUTHORIZED_TOKEN)
+                if BFLA_test_case.test_case.result == TestResult.FAIL:
+                    endpoint_failing_tests.append(BFLA_test_case.test_case)
+            if len(endpoint_failing_tests) > 0:
+                failing_tests.extend(endpoint_failing_tests)
+                click.echo(" 🚨")
+            else:
+                click.echo(" ✅")
+        return failing_tests
+    
+    def run_BFLA_attack_through_request_payloads(self):
+        failing_tests = []
+        for endpoint in self.api_spec.authorized_endpoints:
+            if not endpoint.has_request_payload:
+                continue 
             click.echo(f"    {endpoint.method.upper()} {endpoint.base_url + endpoint.path.path}", nl=False)
             self.auth_tests += 1
             BFLA_test_case = BFLATestCaseRunner(
@@ -83,4 +150,3 @@ class TestBFLA:
             else:
                 click.echo(" ✅")
         return failing_tests
-            
