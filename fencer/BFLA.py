d6a08a6cb4014d16a5361ef66e1df360745ff79d
Eric-0522 <wfps4710@gmail.com>
2024-03-01 14:18:32 +0800

    Complete the BFLA.py module

--- fencer/BFLA.py
+++ fencer/BFLA.py
@@ -9,16 +9,16 @@ from jsf import JSF
 
 from .api_spec import Endpoint, fake_parameter, APISpec
 from .test_case import TestResult, TestCase, AttackStrategy, TestDescription, HTTPMethods, VulnerabilitySeverityLevel
-
-AUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhYmNAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDgzMjQ5MzUsImV4cCI6MTcwODkyOTczNX0.'\
-                   'BjErFScOIk_9FXGVF2xkcLOnHlJMTB5G7Esi31GYRJpq2bItHtSVQzA6eJ4X96fGPnM0qrbVeUHqVM-bE96YMyRirAtKGUVJrivaLwOniJEdOOQ2-NPqSut'\
-                   'bzWTmWgIQfZUoh_1q0gSVS13k-dw5963kUlGeL5d5XZuSIvO0lVufjCoo7ASJOu39RZ2mv109ig-QuX5FOnDcKM9Zx1kh9gcJBoahZyw4khebVDN5M7Z39qE0'\
-                   'c1EhuJyZ34BXy0Vgq5HvuBC0pdKfR7c9X3tTmK9vemxyqTM-ui1TU_ftr_1fOBOk9BIy7lyj3Zh49Mi9t4xhbUOIk3wfG3qMx3BaHQ'
-
-UNAUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjYmFAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDgzMjY3NTYsImV4cCI6MTcwODkzMTU1Nn0.'\
-                     'bCnedndWp2M1W2NLpke_J9X1AO_5h7we4lZDcT7In0J3AqCGdVNIJKllTV6VJA9c_5CXN3cvlfpaZiCB17pMH1PUAaVKtKTvSQQNIGmgNJ--WZN--30Sp00'\
-                     'kVc4-szaY8iR4PUZFw4CZO9PGBmpc-9nA4jUMR3Esj5DLE8ghpq1Gf7mYC1x3PVabSyvTk7JnRIfxnNOKlZb9B-0XiKRdU88URDiI9muHUgF8WC9EnNaYGhQO'\
-                     '-f1O-O5dJM2iQuKhvJX2GhBtiShxIXr618ImISgQEESG0XnVMnmOiKl69hfkgT8aVdS2KYR8Wg4MaXc5m2NgDV6l05VtCrW95XSSbQ'
+# cba token
+AUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjYmFAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDkxNzg2MDksImV4cCI6MTcwOTc4MzQwOX0.mgvHiG'\
+                   'uof2hfxlZ0eTRVcr_U-s3BW0DgDqZEUJCTT6ymWerwaCxbZftefgwrQaCOw1i7Ly2kLJivWO1Kn1Hz6UANCVH-Fpfb8n7hu3WgrG4LUj035h8v5QpZUpDAJhNhu'\
+                   'oqSR_rwWXuxtvxHRIUgCnkls2BpB3TIcqfcaURrl3l_ujLR4T-UPH0bRc9mJ-6jT4Sub19baHD5ZXFKKIrgQ1G4GfxpoS4aeVLkvESQB-Kw3h2jsSovL37mHboH'\
+                   'UR1MkosFaPkdfu-bVk4qY4rul_8hkQE6bbBj_Juv0c_9zziK1hsNXTjgsNfoqGAZUgENsPDObl-mTka2a9MD9SsNSw'
+# hacker token
+UNAUTHORIZED_TOKEN = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJoYWNrZXJAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDg5MjUxMzUsImV4cCI6MTcwOTUyOTkzNX0.'\
+                     'MCrnDpUn349qlblgTsdOaEfVYdR5tGYYHWqCj-yoabgxVeIi9MGjylyCkFdxjYVv5ghjOv6bnaps8FUbtlkfAAdtQODO2SHBMNSSPDcdXF3aUexIinkgvbPSSB'\
+                     '8EXnJjXc38GLmhyUYqVvObE4YDzfvWTshNUAZzjbzxw2hx3tDXWE1qAz8eQD5UBEvPgK4xr6q7qIL6vcWDBPgJOsKVdbsE-4VqohFw0X0D52WDE-brdF_NTEE-'\
+                     'pVrIiFHtJe39lStzIgY3syWxtG8sppiLoRl5dOxWFc6fcQGg6N6DVyAVDiSmD8OgBZw7wwhyjF9sBuSLtm1dvjYhmEJpWhXY6Q'
 
 @dataclass
 class BFLAEndpoint:
@@ -38,17 +38,16 @@ class BFLAEndpoint:
         urls = []
         for param in self.endpoint.path.path_params_list:
             for param_scheams in self.endpoint.path.path_params_schemas:
-                print(self.fake_param_strategy(param_scheams['schema']))
-                path = self.endpoint.path.path.replace(f'{{{param}}}', str(self.fake_param_strategy(param_scheams['schema'])))
+                replace_param = str(self.fake_param_strategy(param_scheams['schema']))
+                path = self.endpoint.path.path.replace(f'{{{param}}}', replace_param)
                 urls.append(self.endpoint.base_url + path)
         return urls
 
     def gets_url_with_test_path_params(self):
         urls = []
         if self.endpoint.path.has_path_params():
-            urls.extend(self.create_fake_parameter_with_path_position())
-            if self.endpoint.has_required_query_params():
-                pass
+            if self.endpoint.has_required_path_params():
+                urls.extend(self.create_fake_parameter_with_path_position())
         for url in urls:
             yield url
 
@@ -67,25 +66,31 @@ class BFLATestCaseRunner:
         self.resolve_test_result()
 
     def resolve_test_result(self):
-        # If the server fails to respond, endpoint is protected and there's no possibility for exploit,
-        # but we can break the server, so we give it a medium severity
-        if self.response is None:
-            self.test_case.result = TestResult.FAIL
-            self.test_case.severity = VulnerabilitySeverityLevel.MEDIUM
-            # If response status code is 401 or 403, it's all good
+        """
+        try:
+            print(self.response.status_code," ",self.response.json())
+        except JSONDecodeError:
+            print('Response format is not json.')
+        """
+        # If response status code is 401 or 403, it's all good
         if self.response.status_code in [401, 403]:
             self.test_case.result = TestResult.SUCCESS
             self.test_case.severity = VulnerabilitySeverityLevel.ZERO
         # If the response status code is in the 2xx status code group, it's pretty bad
-        elif self.response.status_code >= 200 < 300:
+        elif 200 <= self.response.status_code < 300:
             self.test_case.result = TestResult.FAIL
             self.test_case.severity = VulnerabilitySeverityLevel.HIGH
+        # If the server fails to respond, endpoint is protected and there's no possibility for exploit,
+        # but we can break the server, so we give it a medium severity
+        elif self.response is None:
+            self.test_case.result = TestResult.SUCCESS
+            self.test_case.severity = VulnerabilitySeverityLevel.ZERO
         # In all other cases, the response isn't successful, but it's still
         # doing some processing, and that can be leveraged by hackers, so we
         # assign it a high severity
         else:
-            self.test_case.result = TestResult.FAIL
-            self.test_case.severity = VulnerabilitySeverityLevel.HIGH
+            self.test_case.result = TestResult.SUCCESS
+            self.test_case.severity = VulnerabilitySeverityLevel.ZERO
         self.test_case.ended_test()
 
 class TestBFLA:
@@ -95,10 +100,9 @@ class TestBFLA:
     def run_BFLA_attack_through_path_parameters(self):
         failing_tests = []
         for endpoint in self.api_spec.authorized_endpoints:
-            if not endpoint.has_path_params():
+            if not endpoint.has_path_params() or endpoint.method == 'get':
                 continue
             BFLA_attack = BFLAEndpoint(endpoint)
-            #print(BFLA_attack.fake_param_strategy)
             endpoint_failing_tests = []
             click.echo(f"    {endpoint.method.upper()} {endpoint.base_url + endpoint.path.path}", nl=False)
             for url in BFLA_attack.gets_url_with_test_path_params():
@@ -109,7 +113,7 @@ class TestBFLA:
                         test_target="Broken_Function_Level_Authorization_attack",
                         description=TestDescription(
                             http_method=getattr(HTTPMethods, endpoint.method.upper()),
-                            url=endpoint.safe_url, base_url=endpoint.base_url, path=endpoint.path.path,
+                            url=url, base_url=endpoint.base_url, path=endpoint.path.path,
                             payload=(
                                 endpoint.generate_safe_request_payload()
                                 if endpoint.has_request_payload() else None
@@ -117,7 +121,7 @@ class TestBFLA:
                         )
                     )
                 )
-                BFLA_test_case.run(AUTHORIZED_TOKEN)
+                BFLA_test_case.run(UNAUTHORIZED_TOKEN)
                 if BFLA_test_case.test_case.result == TestResult.FAIL:
                     endpoint_failing_tests.append(BFLA_test_case.test_case)
             if len(endpoint_failing_tests) > 0:
@@ -130,7 +134,7 @@ class TestBFLA:
     def run_BFLA_attack_through_request_payloads(self):
         failing_tests = []
         for endpoint in self.api_spec.authorized_endpoints:
-            if not endpoint.has_request_payload:
+            if endpoint.method == 'get':
                 continue 
             click.echo(f"    {endpoint.method.upper()} {endpoint.base_url + endpoint.path.path}", nl=False)
             self.auth_tests += 1
@@ -148,7 +152,7 @@ class TestBFLA:
                     )
                 )
             )
-            BFLA_test_case.run(AUTHORIZED_TOKEN)
+            BFLA_test_case.run(UNAUTHORIZED_TOKEN)
             if BFLA_test_case.test_case.result == TestResult.FAIL:
                 failing_tests.append(BFLA_test_case.test_case)
                 click.echo(" 🚨")
